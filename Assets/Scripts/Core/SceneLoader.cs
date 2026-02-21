16ad38e93dc7413c660457ae52825a967678036a
Thatpugishere <162159592+Thatpugishere@users.noreply.github.com>
2026-02-20 22:59:28 -0800

    HW5 Complete

--- Assets/Scripts/Core/SceneLoader.cs
+++ Assets/Scripts/Core/SceneLoader.cs
@@ -3,6 +3,7 @@ using Core;
 
 public class SceneLoader : MonoBehaviour
 {
+    // This method can be called to load a scene by name instead of using UnityEngine.SceneManagement directly
     public void LoadScene(string sceneName)
     {
         GameManager.Instance.LoadScenebyName(sceneName);
