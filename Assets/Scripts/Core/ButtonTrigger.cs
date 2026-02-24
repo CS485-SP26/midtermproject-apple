74801685859104ebdf11854ace5464e5aa3b010d
Jespav99 <spave001@csusm.edu>
2026-02-24 00:56:37 -0800

    Initial commit

--- Assets/Scripts/Core/ButtonTrigger.cs
+++ Assets/Scripts/Core/ButtonTrigger.cs
@@ -1,13 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Character; 
+using Core;
 
 public class ButtonTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject ButtonUI;
+    [SerializeField] private string sceneToLoad;
+    private Button button;
 
     private void Start()
     {
         ButtonUI.SetActive(false);
+        // Get the Button component
+        button = ButtonUI.GetComponentInChildren<Button>();
+        if (button != null)
+        {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() =>
+            {
+                if (GameManager.Instance != null)
+                    GameManager.Instance.LoadScenebyName(sceneToLoad);
+                else
+                    Debug.LogWarning("GameManager instance is null!");
+            });
+        }
     }
     
     public void OnTriggerEnter(Collider other)
