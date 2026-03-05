3c1bca3539d2f096389014fc18efa17cda68085a
toricess691 <toricess691@gmail.com>
2026-03-05 14:31:02 -0800

    Seasons
    Added the files but I have one error. I will fix it when I get home later today.

--- Assets/Scripts/Farming/Plant.cs
+++ Assets/Scripts/Farming/Plant.cs
@@ -6,18 +6,25 @@ public class Plant : MonoBehaviour
 {
     public enum PlantState {Planted, Growing, Mature, Whithered}
     public PlantState currentState = PlantState.Planted;
+
     [SerializeField] private GameObject plantedModel;
     [SerializeField] private GameObject growingModel;
     [SerializeField] private GameObject matureModel;
     [SerializeField] private GameObject whitheredModel;
 
+    [SerializeField] private SeasonManager seasonManager; // call instances
+
     private int dayGrown = 0;
     private int daysToMature = 3;
+    private int maxWitherLevel = 3;
+    private int witherLevel = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         UpdateVisual();
     }
+    
     public void OnDayPassed(bool wasWatered)
     {
         if(currentState == PlantState.Mature || currentState == PlantState.Whithered)
@@ -39,6 +46,7 @@ public class Plant : MonoBehaviour
         }
         UpdateVisual();
     }
+
     public void UpdateVisual()
     {
         //Hide All Model
@@ -60,4 +68,28 @@ public class Plant : MonoBehaviour
     {
         return currentState == PlantState.Mature;
     }
+
+
+    
+    // this will set the witherrate based on the season. should this be called in to change the daysToMature??
+    private int getSeasonWither()
+    {
+        var currentSeason = seasonManager.GetCurrentSeason();
+
+        switch (currentSeason)
+        {
+            case Season.SeasonType.Spring: 
+                return 2;
+            case Season.SeasonType.Summer: 
+                return 1;
+            case Season.SeasonType.Autumn: 
+                return 1;
+            case Season.SeasonType.Winter: 
+                return 0;
+            default: 
+                return 1;
+        }
+    }
+
+    
 }
